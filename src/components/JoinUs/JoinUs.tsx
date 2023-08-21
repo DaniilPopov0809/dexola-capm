ad24071ef898876d1246bf735b4ff07bb7e987ac
Daniil Popov <x6uhrox@gmail.com>
2023-08-21 22:58:01 +0300

    ADDED: joinUs styles, titles.scss UPD: variables.scss

--- src/components/JoinUs/JoinUs.tsx
+++ src/components/JoinUs/JoinUs.tsx
@@ -1,20 +1,27 @@
 import RegistrationForm from "../RegistrationForm/RegistrationFormForm";
+import styles from "./JoinUs.module.scss";
 
 const JoinUs = () => {
   return (
-    <section>
-      <h2>
-        <span>Join Us</span>
-        <span>03</span>
-      </h2>
-      <h3>Experience the Power of StarRunner</h3>
-      <p>
-        Join Our Community and Embark on a Journey of Opportunities. Discover
-        the full range of our services that cater to your needs. We've got you
-        covered with top-notch solutions.
-      </p>
-      <RegistrationForm />
-    </section>
+    <section className={styles.joinUs}>
+    <h2 className={`title__h2 ${styles.joinUs__title}`}>
+      <span className={styles.joinUs__titleText}>Join Us</span>
+      <span className={styles.joinUs__titleNumber}>03</span>
+    </h2>
+    <div className={styles.joinUs__wrap}>
+    <div className={styles.joinUs__descriptionWrap}>
+    <h3 className={`title__h3 ${styles.joinUs__subtitle}`}>
+      Experience the Power of StarRunner
+    </h3>
+    <p className={styles.joinUs__description}>
+      Join Our Community and Embark on a Journey of Opportunities. Discover the
+      full range of our services that cater to your needs. We've got you covered
+      with top-notch solutions.
+    </p>
+    </div>
+    <RegistrationForm />
+    </div>
+  </section>
   );
 };
 
