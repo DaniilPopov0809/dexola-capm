a8e5289058c51441ba1458cd769c5eb0bf3b2b3c
Daniil Popov <x6uhrox@gmail.com>
2023-08-21 20:50:09 +0300

    ADDED: markup JoinUs

---
+++ src/components/JoinUs/JoinUs.tsx
@@ -0,0 +1,21 @@
+import RegistrationForm from "../RegistrationForm/RegistrationFormForm";
+
+const JoinUs = () => {
+  return (
+    <section>
+      <h2>
+        <span>Join Us</span>
+        <span>03</span>
+      </h2>
+      <h3>Experience the Power of StarRunner</h3>
+      <p>
+        Join Our Community and Embark on a Journey of Opportunities. Discover
+        the full range of our services that cater to your needs. We've got you
+        covered with top-notch solutions.
+      </p>
+      <RegistrationForm />
+    </section>
+  );
+};
+
+export default JoinUs;
