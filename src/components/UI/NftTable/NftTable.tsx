27b197ede233e39d8389b42a0522a1e92d016c2c
Daniil Popov <x6uhrox@gmail.com>
2023-08-27 13:42:18 +0300

    FIX: Features pictures

--- src/components/UI/NftTable/NftTable.tsx
+++ src/components/UI/NftTable/NftTable.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import nftMockData from "../../../data/NftMockData";
+import nftMockData from "../../../data/nftMockData";
 import NftTableRow from "../NftTableRow/NftTableRow";
 import NftTableRowMobile from "../NftTableRowMobile/NftTableRowMobile";
 import styles from "./NftTable.module.scss";
