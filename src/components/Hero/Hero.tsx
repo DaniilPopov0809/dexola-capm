900f1921ae027d93ea2d030a89a8db676cf6baa9
Daniil Popov <x6uhrox@gmail.com>
2023-09-04 10:20:37 +0300

    UPD: SkipButton, Animation, Hero img x3

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -9,15 +9,6 @@ const Hero = () => {
         <ul className={styles.hero__list}>
           <li className={styles.hero__item}>
             {heroMockData[0].map(({ mobile, tab_desk }) => (
-              // <img
-              //   key={item.x1}
-              //   srcSet={`${item.x1} 1x,
-              // ${item.x2} 2x,
-              // ${item.x3} 3x`}
-              //   src={item.x1}
-              //   alt="Hero image"
-              //   className={styles.hero_imageLeft}
-              // />
               <div className={styles.hero_imageContainerLeft}>
                 <picture key={mobile.x1}>
                   <source
@@ -53,45 +44,36 @@ const Hero = () => {
           </li>
           <li className={styles.hero__item}>
             {heroMockData[1].map(({ mobile, tab_desk }) => (
-              // <img
-              //   key={item.x1}
-              //   srcSet={`${item.x1} 1x,
-              // ${item.x2} 2x,
-              // ${item.x3} 3x`}
-              //   src={item.x1}
-              //   alt="Hero image"
-              //   className={styles.hero_imageRight}
-              // />
               <div className={styles.hero_imageContainerRight}>
-              <picture key={mobile.x1}>
-                <source
-                  srcSet={`
+                <picture key={mobile.x1}>
+                  <source
+                    srcSet={`
               ${tab_desk.x1} 1x,
               ${tab_desk.x2} 2x,
               ${tab_desk.x3} 3x
             `}
-                  media="(min-width: 744px)"
-                />
+                    media="(min-width: 744px)"
+                  />
 
-                <source
-                  srcSet={`
+                  <source
+                    srcSet={`
               ${mobile.x1} 1x,
               ${mobile.x2} 2x,
               ${mobile.x3} 3x
             `}
-                  media="(min-width: 375px)"
-                />
+                    media="(min-width: 375px)"
+                  />
 
-                <img
-                  src={`${mobile.x1}`}
-                  alt="Hero image"
-                  aria-hidden="true"
-                  loading="lazy"
-                  className={styles.hero_image}
-                  width={163}
-                  height={311}
-                />
-              </picture>
+                  <img
+                    src={`${mobile.x1}`}
+                    alt="Hero image"
+                    aria-hidden="true"
+                    loading="lazy"
+                    className={styles.hero_image}
+                    width={163}
+                    height={311}
+                  />
+                </picture>
               </div>
             ))}
           </li>
